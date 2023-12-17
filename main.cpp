45f206457d60467f4b60498dcdb834491abf631f
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-17 13:16:08 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int income;
+    std::cout << "Enter your income: ";
+    std::cin >> income;
+    int tax = 0;
+
+
+    if (income > 50000)
+    {
+        tax += (income - 50000) * 0.3;
+        income = 50000;
+    }
+    if (income > 10000)
+    {
+        tax += (income - 10000) * 0.2;
+        income = 10000;
+    }
+    if (income > 0)
+        tax += income * 0.13;
+    else std::cout << "Incorrect input!!!" << std::endl;
+
+    std::cout << "The tax will be equal: " << tax << std::endl;
+
+
+
+
+
 }
